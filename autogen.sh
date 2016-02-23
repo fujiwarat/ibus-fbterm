0704a524f2e4a674c5acdc10d9659f99bd312616
fujiwarat <takao.fujiwara1@gmail.com>
2016-02-23 19:11:08 +0900

    Add NEWS

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 libtoolize --automake --copy --force || exit 1
-aclocal -I . --force || exit 1
+aclocal -I m4 --force || exit 1
 autoheader --force || exit 1
 automake --add-missing --copy --force || exit 1
 autoconf --force || exit 1
